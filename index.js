7f632d6f261fb0b47389a64420340c7c144d969e
shareekaepps <shareekaepps@icloud.com>
2022-07-21 15:15:20 -0400

    accumulator-pattern-lab-done

--- index.js
+++ index.js
@@ -4,16 +4,17 @@
  * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
  */
 function isValid(rolls) {
-  let result = false;          //Step1 determine output, data type and default value
-  for(let roll of rolls){      //Step2 write code for loop
-    if(roll += 1){             //Step 3 Accumulate 
-    result = true;
+  let result = true;
+
+  for(let roll of rolls) {
+    if(typeof roll !== 'number') {
+      return false;
     }
   }
   return result;
 }
-const rolls = [3, 2, 3, 6];
-console.log(isValid(rolls));
+  
+
 /**
  * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
@@ -21,18 +22,14 @@ console.log(isValid(rolls));
  * @returns {*} - The found value or `null`.
  */
 function findValue(rolls, value) {
-  let result = rolls[0];
 
-    for(let i=1;i<rolls.length;i++){
-      let roll = rolls[i];
-      if(roll<result){
-        result = roll;
-      }
-    }
-    return result;
+  for(let i = 1; i < rolls.length; i++){
+    if(rolls[i] == value){
+     return value;
+     }
+   }
+   return null;
   }
-  const rolls = [2, 4, 2, 5, 6];
-  console.log(findValue(rolls));
 
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
@@ -41,34 +38,36 @@ function findValue(rolls, value) {
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
 function filterOutLowValues(rolls, lowest) {
-let result = rolls[9];
+  let allNumbers = [];
 
-  for(let i=0;i<rolls.length;i++){
-    let roll = rolls[i];
-    if(roll<result){
-      result += roll;
+  for(let i = 0; i < rolls.length; i++){
+    if(rolls[i] >= lowest){
+      allNumbers.push(rolls[i]);
     }
   }
-  return result;
+  return allNumbers;
 }
-let rolls = [3, 2, 4, 2, 5, 6, 3];
-console.log(filterOutLowValues(rolls));
+
 /**
  * Returns an object which has rolls as keys and counts as values.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {object} An object where the keys are numbers rolled and the values are the number of times that roll appears in the `rolls` array.
  */
-function getRollCounts(rolls) {}
-let counters = {};                //Step1 set to empty obj
+function getRollCounts(rolls) {
+  let counters = {};
 
-  for(let roll of rolls){       //Step2 write for of loop
-    counters[roll] = 1;         //Step3 accumulate keys and values into obj
+  for(let i = 0; i < rolls.length; i++) {
+    let num = rolls[i];
+      if(!counters[num]) {
+        counters[num] = 1;
+      } else {
+        counters[num] += 1;
+    }
   }
   return counters;
 }
-let rolls = [2, 3, 4];
-console.log(getRollCounts(rolls));
-//const expected = { 2: 1, 3: 1, 4: 1 };
+
+
 
 // Do not change the code below here.
 module.exports = {
